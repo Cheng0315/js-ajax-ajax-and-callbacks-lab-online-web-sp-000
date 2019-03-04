a9c526cf2f816e3b085081c77f00b9f42be2a677
Cheng Vue <csalvue@gmail.com>
2019-03-04 00:08:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,9 @@ $(document).ready(function (){
 });
 
 function searchRepositories() {
-
+  const req = new XMLHttpRequest();
+  let searchTerm = document.getElementById('searchTerm').value;
+  req.addEventListener('load', displayRepositories);
+  req.open('GET', `https://api.github.com/search/repositories?q=${searchTerms}`);
+  req.send();
 }
